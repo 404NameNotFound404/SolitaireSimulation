ed6eb10fd52fc8c60abe65425971af9fc9b3e293
Nathan <ndrees001@csbsju.edu>
2020-10-04 10:33:18 -0500

    Updated .gitignore and added CardStack and the test file

---
+++ CardStack.java
@@ -0,0 +1,40 @@
+package SolitaireSimulation;
+
+/*
+ * 
+ * @Author Nathan Drees
+ * 
+ */
+public class CardStack extends CardCollection
+{
+	super();
+	super(Stack<Card> s);
+	
+	public boolean CheckOrder()
+	{
+		boolean result = true;
+		int i = 1;
+		int j = 0;
+		while((super.cards.elementAt(i)).isFaceUp())
+		{
+			if ((super.cards.elementAt(i)).getValue() < (super.cards.elementAt(j)).getValue())
+			{
+				results = false;
+				i++;
+				j++;
+			}
+		}
+		return reslut;
+	}
+	
+	public void addToStack(Card c)
+	{
+		super.cards.push(c);
+	}
+	
+	public void removeTopCard()
+	{
+		super.cards.pop();
+	}
+	
+}
---
+++ CardStackTest.java
@@ -0,0 +1,24 @@
+package SolitaireSimulation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+class CardStackTest {
+
+	@Test
+	void testCheckOrder() {
+		;
+	}
+	
+	@Test
+	void testAddToStack() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testremoveTopCard() {
+		fail("Not yet implemented");
+	}
+
+}
