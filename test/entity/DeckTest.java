374e8fd3069e8af991f67e2a5cd800b87c134962
Andrew Heroux <herouxandrew@gamil.com>
2020-10-04 17:50:41 -0500

    Add tests for shuffling decks

--- test/entity/DeckTest.java
+++ test/entity/DeckTest.java
@@ -2,6 +2,7 @@ package entity;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.util.Random;
 
 public class DeckTest {
 	
@@ -27,6 +28,70 @@ public class DeckTest {
 		assertFalse(match);
 	}
 	
+	@Test
+	public void testDrawCardTakesFromDeck() {
+		Deck testDeck = new Deck();
+		
+		int randomNum = new Random().nextInt(52);
+		
+		for (int i = 0; i < randomNum; i++) {
+			testDeck.drawCard();
+		}
+		
+		assertTrue(testDeck.getCardStack().size() == 52 - randomNum);
+	}
 	
+	
+	
+	@Test
+	public void testShuffleMakesRandomDeckOneShuffle() {
+		Deck testDeck = new Deck();
+		Deck testDeckShuffled = new Deck();
+		testDeckShuffled.shuffleDeck();
+		
+		int i = 0;
+		boolean cardMatch = true;
+		while (i < 52 && cardMatch) {
+			if (!testDeck.drawCard().toString().equals(testDeckShuffled.drawCard().toString())) {
+				cardMatch = false;
+			}
+			i++;
+		}
+		assertFalse(cardMatch);
+	}
+	
+	@Test
+	public void testShuffleMakesRandomDeckMatchingDecks() {
+		Deck testDeck = new Deck();
+		Deck testDeckMatch = new Deck();
+		
+		int i = 0;
+		boolean cardMatch = true;
+		while (i < 52 && cardMatch) {
+			if (!testDeck.drawCard().toString().equals(testDeckMatch.drawCard().toString())) {
+				cardMatch = false;
+			}
+			i++;
+		}
+		assertTrue(cardMatch);
+	}
+	
+	@Test
+	public void testShuffleMakesRandomDeckTwoShuffles() {
+		Deck testDeckShuffled0 = new Deck();
+		Deck testDeckShuffled1 = new Deck();
+		testDeckShuffled0.shuffleDeck();
+		testDeckShuffled1.shuffleDeck();
+		
+		int i = 0;
+		boolean cardMatch = true;
+		while (i < 52 && cardMatch) {
+			if (!testDeckShuffled0.drawCard().toString().equals(testDeckShuffled1.drawCard().toString())) {
+				cardMatch = false;
+			}
+			i++;
+		}
+		assertFalse(cardMatch);
+	}
 
 }
