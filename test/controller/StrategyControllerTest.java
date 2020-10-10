790c8693321debcadf47908c14776de053b357e8
mia-li-z <mli001@csbsju.edu>
2020-10-11 03:15:31 +0800

    modifying StrConTest

--- test/controller/StrategyControllerTest.java
+++ test/controller/StrategyControllerTest.java
@@ -2,23 +2,31 @@ package controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import controller.StrategyController;
-
 import org.junit.jupiter.api.Test;
+import org.junit.Before;
 
 public class StrategyControllerTest {
+	
+	public boolean result;
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		result =  StrategyController.StaticStrategyController.playGame();
+	}
+
+	
 	
  	@Test
 	public void testPlayGame() {
  		
- 		boolean result = StrategyController.StaticStrategyController.playGame();
- 		System.out.print(result);
  		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testMoveTableuToFoundation() {
-//		StrategyController.StaticStrategyController.moveTableauToFoundation();
+		
 		fail("Not yet implemented");
 	}
 	
@@ -26,11 +34,22 @@ public class StrategyControllerTest {
 	public void testGetTurns() {
 		assertEquals(0,StrategyController.StaticStrategyController.getTurns());
 		
-		boolean result = StrategyController.StaticStrategyController.playGame();
-		if (result == false);
+		if (result == false)
+		{
 			assertEquals(3, StrategyController.StaticStrategyController.getTurns());
 		
+		}
+		else
+		{
+			assertFalse(StrategyController.StaticStrategyController.getTurns() == 3);
+		}
 		
+	}
+	
+	@Test
+	public void testGetTime() {
+ 		
+ 		assertFalse(StrategyController.StaticStrategyController.getTime() == 0);
 	}
 	
 }
