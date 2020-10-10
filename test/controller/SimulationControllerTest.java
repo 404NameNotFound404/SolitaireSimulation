58bc82b589f1a8011f4d936670a6bfd7ea5d79c5
Nathan <ndrees001@csbsju.edu>
2020-10-10 11:56:55 -0500

    Added test classes for controllers, no tests implemented

---
+++ test/controller/SimulationControllerTest.java
@@ -0,0 +1,28 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SimulationControllerTest {
+
+	@Test
+	void testGetWinPercentage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAverageTurns() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testStartSimulation() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testgetAverageTime() {
+		fail("Not yet implemented");
+	}
+}
